6769ec2d1326a0ec157311c79ec5b3e946d99988
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-07-17 23:49:12 -0600

    remove: Se ha eliminado estos archivos porque estaban en un nivel de carpeta equivocado

--- act4-b3-taller/src/utils/readline.ts
+++
@@ -1,7 +0,0 @@
-import readline from "readline/promises";
-import { stdin as input, stdout as output } from "process";
-
-export const rl=readline.createInterface({
-    input,
-    output
-});
--- act4-b3-taller/src/utils/validators.ts
+++
@@ -1,6 +0,0 @@
-export function isAllowedEmailDomain(email: string): boolean {
-    if (!email || typeof email !== 'string') return false;
-    const lower = email.toLowerCase().trim();
-    const allowed = ["@gmail.com", "@outlook.com", "@yahoo.com"];
-    return allowed.some(d => lower.endsWith(d));
-}
